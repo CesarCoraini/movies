3ae52b6d1537f220bc333fa57db549241dc71a8b
Coraini <coraini1990@gmail.com>
2023-05-04 16:46:26 +0100

    first commit

---
+++ src/main/java/dev/coraini/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package dev.coraini.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
