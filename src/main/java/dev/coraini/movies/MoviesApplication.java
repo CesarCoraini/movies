4e6951718d4edc51120f9b4bc9617df006bb6d00
Coraini <coraini1990@gmail.com>
2023-05-04 22:47:19 +0100

    Movie and Review class added

--- src/main/java/dev/coraini/movies/MoviesApplication.java
+++ src/main/java/dev/coraini/movies/MoviesApplication.java
@@ -2,8 +2,11 @@ package dev.coraini.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MoviesApplication {
 
 	public static void main(String[] args) {
